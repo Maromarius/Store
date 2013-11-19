91db86fb8e5497bacc4666ba0fb919b9abf875a3
Maromarius <ramikuret@gmail.com>
2013-11-19 15:38:21 -0500

    adad

---
+++ src/com/gamestore/dao/ItemMapper.java
@@ -0,0 +1,125 @@
+package com.gamestore.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.gamestore.model.Item;
+
+public class ItemMapper {
+
+	
+	private static ItemMapper soleInstance = null;
+	
+	public static ItemMapper getInstance() {
+		if (soleInstance == null) soleInstance =  new ItemMapper();
+		
+		return soleInstance;
+	}
+	
+	private Map<Integer, Item> item = new HashMap<Integer, Game>(); 
+		
+	public void addGameToMap(Game arg) {
+		soleInstance.game.put(arg.getID(), arg);
+	}
+	
+	public Game[] getAll() {
+		
+		List<Game> list = new ArrayList<Game>();
+		String query = "select * from Games";
+		ResultSet rs = DBAccess.getInstance().ExecuteQuery(query);
+		try {
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				Game gm = game.get(id);
+				if (gm == null){
+					String name = rs.getString("name");
+					double price = rs.getDouble("price");
+					String desc = rs.getString("description");
+					int qty = rs.getInt("quantity");
+					String cat = rs.getString("category");
+					String image = rs.getString("image");
+
+					gm = new Game(id,name,desc,price,qty,cat, image);
+				}
+				list.add(gm);
+			}
+			// Add All New Games
+			List<DomainObject> addedObjects = UOW.getCurrent().getAllNew();
+			for(DomainObject item : addedObjects) {
+				list.add((Game)item);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list.toArray(new Game[list.size()]);
+	}
+	
+	private DBStatus GetStatus() {
+		
+		return null;
+	}
+
+	public Game get(int key) {
+		Game gm =  (Game) soleInstance.game.get(key);
+		if (gm == null) {
+			try{
+				String query = "select * from Games where id =" + key; 
+				ResultSet rs = DBAccess.getInstance().ExecuteQuery(query);
+				rs.next();
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				double price = rs.getDouble("price");
+				String desc = rs.getString("description");
+				int qty = rs.getInt("quantity");
+				String cat = rs.getString("category");
+				String image = rs.getString("image");
+				
+				gm = new Game(id,name,desc,price,qty, cat, image);
+				this.addGameToMap(gm);
+				}
+			catch (Exception ex) {
+					
+			}
+		}
+		
+		return gm;
+	}
+	
+	public void insert (DomainObject obj) {
+		Game gm = (Game) obj;
+		//DB Insert 
+		String values = "'" + gm.getName() + "','" + gm.getPrice() + "','" + gm.getDescription() + "','" + gm.getQty() + "','" + gm.getCategory() + "','" + gm.getImage() + "'";
+		String query = "INSERT INTO Games (name,price,description,quantity,category, image) VALUES (" + values + ")";
+		int  id = DBAccess.getInstance().ExecuteInsert(query);
+		// Set id from DB
+		gm.setID(id);
+		soleInstance.game.put(gm.getID(), gm);
+	}
+	
+	public void update(DomainObject obj) {
+		Game gm = (Game) obj;
+		String query = "UPDATE Games SET name='" + gm.getName() + "',price='" + gm.getPrice() + "',description='" + gm.getDescription() + "', quantity='" + gm.getQty() + "', category='" + gm.getCategory()  + "', image='" + gm.getImage()  + "' WHERE id=" + gm.getID();
+		DBAccess.getInstance().Execute(query);
+	}
+	public void delete(int key) {
+		String query = "DELETE FROM Games WHERE id=" + key;
+		DBAccess.getInstance().Execute(query);
+		soleInstance.game.remove(key);
+	}
+
+	public void CleanAll() {
+		for (Map.Entry<Integer, Game> entry : game.entrySet()) {
+			entry.getValue().setStatus(DBStatus.CLEAN);
+		}
+		
+	}
+	
+}
+
---
+++ src/com/gamestore/dao/UnitofWork.java
@@ -0,0 +1,97 @@
+package com.gamestore.dao;
+import java.lang.Object;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+
+import org.junit.Assert;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class UnitofWork {
+	private List<DomainObject> newObjects = new ArrayList();
+	private List<DomainObject> dirtyObjects = new ArrayList();
+	private List<DomainObject> removedObjects = new ArrayList();
+	private static UnitofWork current = null;
+	
+	
+	public static UnitofWork getCurrent() {
+		if (current == null)
+		{
+			current = new UnitofWork();
+		}
+		return current;
+	}
+	
+	
+	public void registerNew(DomainObject obj) {
+		Assert.assertNotNull("id not null", obj.getID());
+		Assert.assertTrue("object not dirty", !dirtyObjects.contains(obj));
+		Assert.assertTrue("object not removed", !removedObjects.contains(obj));
+		Assert.assertTrue("object not already registered new", !newObjects.contains(obj));
+		newObjects.add(obj);
+	}
+	
+	public void registerDirty(DomainObject obj) {
+		Assert.assertNotNull("id not null", obj.getID());
+		Assert.assertTrue("object not removed", !removedObjects.contains(obj));
+		if (!dirtyObjects.contains(obj) && !newObjects.contains(obj)) {
+			dirtyObjects.add(obj);
+		}
+	}
+	
+	public void registerRemoved(DomainObject obj) {
+		Assert.assertNotNull("id not null", obj.getID());
+		if (newObjects.remove(obj)) return;
+		dirtyObjects.remove(obj);
+		if (!removedObjects.contains(obj)) {
+			removedObjects.add(obj);
+		}
+	}
+	
+	public void registerClean(DomainObject obj) {
+		Assert.assertNotNull("id not null", obj.getID());
+	}
+	
+	public void commit() 
+	{
+		insertNew();
+		updateDirty();
+		deleteRemoved();
+		newObjects.clear();
+		dirtyObjects.clear();
+		removedObjects.clear();
+		GameMapper.getInstance().CleanAll();	
+	}
+	private void insertNew() {
+		for (Iterator objects = newObjects.iterator(); objects.hasNext();) {
+			DomainObject obj = (DomainObject) objects.next();
+			// DB Insert
+			GameMapper.getInstance().insert(obj);
+		}
+	}
+	
+	private void updateDirty() {
+		for (Iterator objects = dirtyObjects.iterator(); objects.hasNext();) {
+			DomainObject obj = (DomainObject) objects.next();
+			// DB Update
+			GameMapper.getInstance().update(obj);
+		}
+	}
+	
+	private void deleteRemoved() {
+		for (Iterator objects = removedObjects.iterator(); objects.hasNext();) {
+			DomainObject obj = (DomainObject) objects.next();
+			// DB Delete
+			GameMapper.getInstance().delete(obj.getID());
+		}
+	}
+	public List<DomainObject> getAllNew() {
+		return this.newObjects;
+	}
+
+}
